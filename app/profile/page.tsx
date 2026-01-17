ac68333a476bcbe6f234f53d8bc30967bfc4d71c
tze-xuan <ktzexuan@gmail.com>
2026-01-17 17:18:58 +0800

    Add profile route

---
+++ app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function GroupsPage() {
+  return (
+    <div className='flex min-h-full flex-col items-center justify-center p-8'>
+      <h1 className='mb-8 text-4xl font-bold'>Profile</h1>
+    </div>
+  );
+}
