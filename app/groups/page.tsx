78158823cf6ea9f26004d491f633e1bab12c63ff
Kah Ho <sweekahho@gmail.com>
2026-01-17 15:13:32 +0800

    Setup project-specific template

---
+++ app/groups/page.tsx
@@ -0,0 +1,7 @@
+export default function GroupsPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-full p-8">
+      <h1 className="text-4xl font-bold mb-8">Create Groups</h1>
+    </div>
+  )
+}
